634f7c0946403ab9eda71676821064694acb04e3
[rrenat358] <[rrenat358@gmail.com]>
2022-03-01 21:50:58 +0300

    printThreeWords()

---
+++ Lesson01/Lesson01.java
@@ -0,0 +1,20 @@
+package Lesson01;
+
+public class Lesson01 {
+    public static void main(String[] args) {
+        printThreeWords();
+
+
+
+
+
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+}
