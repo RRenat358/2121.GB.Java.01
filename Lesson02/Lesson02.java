bb1d50f368f3210dcaff4a5b1886dd75eabdef09
[rrenat358] <[rrenat358@gmail.com]>
2022-03-02 21:06:56 +0300

    checkSum() == ok

--- Lesson02/Lesson02.java
+++ Lesson02/Lesson02.java
@@ -1,4 +1,20 @@
 package Lesson02;
 
 public class Lesson02 {
+    public static void main(String[] args) {
+        System.out.println("––––––––––––––––––––––––––––––");
+        System.out.println(checkSum(2, 10));
+        System.out.println("––––––––––––––––––––––––––––––");
+
+
+
+    }
+
+    private static boolean checkSum(int a, int b) {
+        int c = a+b;
+        if (c >= 10 && c <= 20) {
+            return true;
+        }
+        return false;
+    }
 }
