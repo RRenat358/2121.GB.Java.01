9ac2cfe84fc6b7715fc59ca2a3d377b83dedb59f
[rrenat358] <[rrenat358@gmail.com]>
2022-03-02 21:34:30 +0300

    checkEven == ok

--- Lesson02/Lesson02.java
+++ Lesson02/Lesson02.java
@@ -3,18 +3,30 @@ package Lesson02;
 public class Lesson02 {
     public static void main(String[] args) {
         System.out.println("––––––––––––––––––––––––––––––");
-        System.out.println(checkSum(2, 10));
+        int checkSumA = 20;
+        int checkSumB = 10;
+        checkSum(checkSumA, checkSumB);
         System.out.println("––––––––––––––––––––––––––––––");
-
+        checkEven(0);
+        System.out.println("––––––––––––––––––––––––––––––");
 
 
     }
 
-    private static boolean checkSum(int a, int b) {
-        int c = a+b;
+
+    private static void checkSum(int a, int b) {
+        int c = a + b;
         if (c >= 10 && c <= 20) {
-            return true;
-        }
-        return false;
+            System.out.println(a + " + " + b + " = в пределе с 10 по 20");
+        } else
+            System.out.println(a + " + " + b + " = за пределом с 10 по 20");
+    }
+
+    private static void checkEven(int a) {
+        if ((a % 2) == 0) {
+            System.out.println(a + " = положительное");
+        } else
+            System.out.println(a + " = отрицательное");
+
     }
 }
