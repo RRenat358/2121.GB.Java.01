ad1170d20600cc69d12b78c818140139c50a7c38
[rrenat358] <[rrenat358@gmail.com]>
2022-03-11 21:14:08 +0300

    2113

--- Lesson04/TicTacToe.java
+++ Lesson04/TicTacToe.java
@@ -1,26 +1,45 @@
 package Lesson04;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
         //======================================================================
-        int arrLength = 3;
-        System.out.println("––– 04 –––––––––––––––––––––––––––");
-        System.out.println("Заполнить диагонали квадратного массива");
-        String[][] arraySqDiagonal = new String[arrLength][arrLength];
-        for (int i = 0; i < arraySqDiagonal.length; i++) {
-            for (int j = 0; j < arraySqDiagonal[i].length; j++) {
-                if (i == j || i == arrLength - 1 - j) {
-                    arraySqDiagonal[i][j] = " ⬛ ";
-                } else {
-                    arraySqDiagonal[i][j] = " · ";
-                }
-                System.out.print(arraySqDiagonal[i][j]);
+
+        System.out.println("––– TicTacToe ––––––––––––––––\n");
+//        System.out.println("Заполнить диагонали квадратного массива");
+        System.out.println("\n––––––––––––––––––––––––––––––");
+        //======================================================================
+
+    }
+    public static int arrLength = 3;
+    String[][] arrayTictactoe = new String[arrLength][arrLength];
+
+
+
+
+    public static void arrayTictactoe() {
+        for (int i = 0; i < arrayTictactoe.length; i++) {
+            for (int j = 0; j < arrayTictactoe[i].length; j++) {
+                arrayTictactoe[i][j] = " · ";
+                System.out.print(arrayTictactoe[i][j]);
             }
             System.out.println();
         }
-        System.out.println();
-        //======================================================================
-
 
     }
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void moveHuman() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты = X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        arrayTictactoe[y][x] = DOT_X;
+
+    }
+
 }
