9381522f7773ce47f5a3a72e9b47b1bc69914e9f
[rrenat358] <[rrenat358@gmail.com]>
2022-03-12 22:16:08 +0300

    2215

--- Lesson04/TicTacToe.java
+++ Lesson04/TicTacToe.java
@@ -1,24 +1,36 @@
+/*
 package Lesson04;
 
 import java.util.Scanner;
 
 public class TicTacToe {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
         //======================================================================
-
         System.out.println("––– TicTacToe ––––––––––––––––\n");
 //        System.out.println("Заполнить диагонали квадратного массива");
+
+        public static final char DOT_EMPTY = '•';
+        public static final char DOT_X = 'X';
+        public static final char DOT_O = 'O';
+        public static char[][] map;
+
+
+
+
+        arrayTictactoe();
+        moveHuman();
+
+
+
         System.out.println("\n––––––––––––––––––––––––––––––");
         //======================================================================
 
-    }
+
     public static int arrLength = 3;
-    String[][] arrayTictactoe = new String[arrLength][arrLength];
+    static String[][] arrayTictactoe = new String[arrLength][arrLength];
 
 
-
-
-    public static void arrayTictactoe() {
+    public static void arrayTictactoe(String[][] arrayTictactoe) {
         for (int i = 0; i < arrayTictactoe.length; i++) {
             for (int j = 0; j < arrayTictactoe[i].length; j++) {
                 arrayTictactoe[i][j] = " · ";
@@ -26,20 +38,39 @@ public class TicTacToe {
             }
             System.out.println();
         }
-
+    }
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
     }
 
     public static Scanner sc = new Scanner(System.in);
+    String moveHuman = "○";
+    String moveComp = "X";
 
-    public static void moveHuman() {
+    public static void isMoveValid() {
+
+        }
+
+    public static void moveHuman(String moveHuman) {
         int x, y;
+
         do {
             System.out.println("Введите координаты = X Y");
             x = sc.nextInt() - 1;
             y = sc.nextInt() - 1;
-        } while (!isCellValid(x, y));
-        arrayTictactoe[y][x] = DOT_X;
+        } while (!isMoveValid(x, y));
+        arrayTictactoe[y][x] = moveHuman;
+    }
+
+    public static void isMoveValid(x,y) {
 
     }
 
 }
+*/
