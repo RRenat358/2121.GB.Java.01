204801bb3016d5d9cdceca63d793b7118f555e1d
rrenat358 <rrenat358@gmail.com>
2022-03-14 13:41:24 +0300

    checkCellNull() = ok

--- Lesson04/TicTacToe.java
+++ Lesson04/TicTacToe.java
@@ -1,10 +1,12 @@
 package Lesson04;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
+    public static Scanner sc = new Scanner(System.in);
+    public static Random random = new Random();
+
     public static int arrMapLength = 3;
     static String[][] arrMap = new String[arrMapLength][arrMapLength];
 
@@ -17,16 +19,24 @@ public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("––– TicTacToe ––––––––––––––––\n");
         System.out.println("Сыграем в крестики-нолики?)");
-
-
         arrMap(arrMapLength);
         arrMapPrint();
-        System.out.println("––––––––––––––––––––––––––––––");
-//        stepHuman();
-//        arrMapPrint();
-        stepComp();
-        arrMapPrint();
 
+
+        while (true) {
+            System.out.println("––––––––––––––––––––––––––––––");
+            stepHuman();
+            arrMapPrint();
+            stepComp();
+            arrMapPrint();
+
+            if (!checkCellNull()) {
+                break;
+            }
+
+        }
+
+        System.out.println();
         System.out.println("\n––––––––––––––––––––––––––––––");
         System.out.println("    Конец игры");
         System.out.println("––––––––––––––––––––––––––––––");
@@ -60,7 +70,6 @@ public class TicTacToe {
         System.out.println();
     }
 
-    public static Scanner sc = new Scanner(System.in);
 
     public static void stepHuman() {
         int x, y;
@@ -74,7 +83,7 @@ public class TicTacToe {
         } while (x < 1 || x > arrMapLength || y < 1 || y > arrMapLength);
         arrMap[x - 1][y - 1] = stepX;
     }
-    public static Random random = new Random();
+
     public static void stepComp() {
         int x, y;
 
@@ -88,25 +97,20 @@ public class TicTacToe {
         } while (x < 1 || x > arrMapLength || y < 1 || y > arrMapLength);
         arrMap[x - 1][y - 1] = stepO; //todo -1 == можно выше в методе не добавлять
     }
-
-
-    public static void isMoveValid() {
-
+    public static boolean checkCellNull() {
+        for (int i = 0; i < arrMapLength; i++) {
+            for (int j = 0; j < arrMapLength; j++) {
+                if (arrMap[i][j] == cellNull) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-/*
-    public static void stepHuman(String stepHuman) {
-        int x, y;
 
-        do {
-            System.out.println("Введите координаты = X Y");
-            x = sc.nextInt() - 1;
-            y = sc.nextInt() - 1;
-        } while (!isMoveValid(x, y));
-        arrayTictactoe[y][x] = stepHuman;
-    }
 
-    public static void isStepValid(x,y) {
+ /*   public static void isStepValid(x,y) {
 
 
     }
