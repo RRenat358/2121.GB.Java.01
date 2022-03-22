13098a47333ad9390cad56d19fce03c8b4188a07
[rrenat358] <[rrenat358@gmail.com]>
2022-03-22 22:32:08 +0300

    create. class Employe

---
+++ lesson05/Employe.java
@@ -0,0 +1,23 @@
+package lesson05;
+
+public class Employe {
+    String name = "-";
+    int age;
+    String phoneNamber = "-";
+    String email = "-";
+    String position = "-";
+    int salary;
+
+    public Employe(String name, int age, String phoneNamber, String email, String position, int salary) {
+        this.name = name;
+        this.age = age;
+        this.phoneNamber = phoneNamber;
+        this.email = email;
+        this.position = position;
+        this.salary = salary;
+    }
+
+
+
+
+}
