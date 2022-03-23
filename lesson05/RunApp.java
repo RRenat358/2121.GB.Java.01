0a208365e7dc29947d27feb49c7d6210028ac0f4
rrenat358 <rrenat358@gmail.com>
2022-03-23 16:51:37 +0300

    1322

--- lesson05/RunApp.java
+++ lesson05/RunApp.java
@@ -7,9 +7,13 @@ public class RunApp {
 
 
         Employe[] persArray = new Employe[5];
-        persArray[0] = new Employe("имя", 5,"имя","имя","имя","имя",5);
+        persArray[0] = new Employe("Татьяна", 28,"+7 800 555 0001","123@gmail.com","Менеджер №01",50000);
+        persArray[1] = new Employe("Татьяна", 41,"+7 800 555 0002","123@gmail.com","Менеджер №02",50000);
+        persArray[2] = new Employe("Татьяна", 33,"+7 800 555 0003","123@gmail.com","Менеджер №03",50000);
+        persArray[3] = new Employe("Татьяна", 42,"+7 800 555 0004","123@gmail.com","Менеджер №04",50000);
+        persArray[4] = new Employe("Татьяна", 25,"+7 800 555 0005","123@gmail.com","Менеджер №05",50000);
 
-
+        System.out.println();
 
 
     }
