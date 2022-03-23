30a1a73bea8590174f245152b10b243efc83cd67
rrenat358 <rrenat358@gmail.com>
2022-03-23 18:13:04 +0300

    1812

--- lesson05/Employe.java
+++ lesson05/Employe.java
@@ -1,12 +1,12 @@
 package lesson05;
 
 public class Employe {
-    String name = "-";
-    int age;
-    String phoneNumber = "-";
-    String email = "-";
-    String position = "-";
-    int salary;
+    String name = "ИМЯ";
+    int age = 1;
+    String phoneNumber = "+7---";
+    String email = "email";
+    String position = "Должность";
+    int salary = 10000;
 
     public Employe() {
     }
@@ -21,12 +21,29 @@ public class Employe {
     }
 
 
-
-
-
     public String employeToString() {
-        return "name = " + name + ", age = " + age + "\n," +
-                phoneNumber + " | " + email + "\n," +
-                position + " | " + salary + " руб.";
+        return name + ", " + age + ", " + phoneNumber + ", " + email + ", " + position + ", " + salary;
     }
+
+    /*
+        public String employeToString2(String name, int age, String phoneNumber, String email, String position, int salary) {
+            return "name = " + name + ", age = " + age + "\n," +
+                    phoneNumber + " | " + email + "\n," +
+                    position + " | " + salary + " руб.";
+        }
+    */
+    public String employeToString2(String name, int age, String phoneNumber, String email, String position, int salary) {
+        return name + ", " + age + ", " + phoneNumber + ", " + email + ", " + position + ", " + salary;
+    }
+
+/*
+    public Employe employeToString3() {
+        System.out.println("name = " + name + ", age = " + age + "\n," +
+                phoneNumber + " | " + email + "\n," +
+                position + " | " + salary + " руб.");
+        return
+    }
+*/
+
+
 }
--- lesson05/RunApp.java
+++ lesson05/RunApp.java
@@ -3,23 +3,20 @@ package lesson05;
 public class RunApp {
     public static void main(String[] args) {
         Employe employe = new Employe();
+        System.out.println("––––––––––––––––––––––––––––––");
         System.out.println(employe.employeToString());
 
-
+        //======================================================================
         Employe[] persArray = new Employe[5];
-        persArray[0] = new Employe("Татьяна", 28,"+7 800 555 0001","123@gmail.com","Менеджер №01",50000);
-        persArray[1] = new Employe("Татьяна", 41,"+7 800 555 0002","123@gmail.com","Менеджер №02",50000);
-        persArray[2] = new Employe("Татьяна", 33,"+7 800 555 0003","123@gmail.com","Менеджер №03",50000);
-        persArray[3] = new Employe("Татьяна", 42,"+7 800 555 0004","123@gmail.com","Менеджер №04",50000);
-        persArray[4] = new Employe("Татьяна", 25,"+7 800 555 0005","123@gmail.com","Менеджер №05",50000);
+        persArray[0] = new Employe("Татьяна", 28, "+78005550001", "123@gmail.com", "Менеджер №01", 50000);
+        persArray[1] = new Employe("Татьяна", 41, "+78005550002", "123@gmail.com", "Менеджер №02", 50000);
+        persArray[2] = new Employe("Татьяна", 33, "+78005550003", "123@gmail.com", "Менеджер №03", 50000);
+        persArray[3] = new Employe("Татьяна", 42, "+78005550004", "123@gmail.com", "Менеджер №04", 50000);
+        persArray[4] = new Employe("Татьяна", 25, "+78005550005", "123@gmail.com", "Менеджер №05", 50000);
 
 
         System.out.println("––––––––––––––––––––––––––––––");
-        System.out.println(persArray[0].age);
-
-        System.out.println("––––––––––––––––––––––––––––––");
-        System.out.println(employe.employeToString(persArray[0].age));
-
+        System.out.println(employe.employeToString2(persArray[0].name, persArray[0].age, persArray[0].phoneNumber, persArray[0].email, persArray[0].position, persArray[0].salary));
         System.out.println("––––––––––––––––––––––––––––––");
     }
 }
