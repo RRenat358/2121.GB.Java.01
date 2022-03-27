9fe15a4c6e5cee6359ca5dcb66326379bdb399a6
[rrenat358] <[rrenat358@gmail.com]>
2022-03-27 22:47:28 +0300

    2247

---
+++ Lesson06/Cat.java
@@ -0,0 +1,18 @@
+package Lesson06;
+
+public class Cat extends Animals{
+    private int CatDistanceTraveled = 50;
+
+
+    public Cat(int distanceTraveled) {
+        super(distanceTraveled);
+//        Cat cat = new Cat(distanceTraveled);
+    }
+
+    @Override
+    public void Run() {
+        System.out.println("Cat run = " + CatDistanceTraveled);
+
+
+    }
+}
