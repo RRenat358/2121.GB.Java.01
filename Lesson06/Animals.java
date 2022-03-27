a6b8150582c998f0cb5b6cef3fef9553076d47a0
[rrenat358] <[rrenat358@gmail.com]>
2022-03-27 22:25:10 +0300

    2225

--- Lesson06/Animals.java
+++ Lesson06/Animals.java
@@ -1,4 +1,19 @@
 package Lesson06;
 
-public class Animals {
+public abstract class Animals {
+    private int distanceTraveled = 0;
+
+    public Animals(int distanceTraveled) {
+        this.distanceTraveled = distanceTraveled;
+    }
+
+    public int getDistanceTraveled() {
+        return distanceTraveled;
+    }
+
+    public void setDistanceTraveled(int distanceTraveled) {
+        this.distanceTraveled = distanceTraveled;
+    }
+
+    public abstract void Run();
 }
