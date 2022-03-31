362f47c2c8d55f1cfe7feab40bbfbb09fa881932
rrenat358 <rrenat358@gmail.com>
2022-03-31 13:01:39 +0300

    Cat -- create

---
+++ Lesson07/Cat.java
@@ -0,0 +1,30 @@
+package Lesson07;
+
+public class Cat {
+    String name = "КотНеизвестный";
+    int appetit = 0;
+
+    public Cat() {
+    }
+
+    public Cat(String name, int appetit) {
+        this.name = name;
+        this.appetit = appetit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAppetit() {
+        return appetit;
+    }
+
+    public void setAppetit(int appetit) {
+        this.appetit = appetit;
+    }
+}
--- Lesson07/RunApp07.java
+++ Lesson07/RunApp07.java
@@ -1,4 +1,25 @@
 package Lesson07;
 
 public class RunApp07 {
+    public static void main(String[] args) {
+        //Ввод дистанций
+        int distanceRun = 130;
+        int distanceSwim = 80;
+
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+        System.out.println("––––––––––––––––––––––––––––––");
+        cat.Run(distanceRun);
+        dog.Run(distanceRun);
+
+        System.out.println("––––––––––––––––––––––––––––––");
+        cat.Swim(distanceSwim);
+        dog.Swim(distanceSwim);
+
+        System.out.println("––––––––––––––––––––––––––––––");
+        System.out.println("В соревнованиях участвовало: " + cat.getAnimalCount() + " животных.");
+
+        System.out.println("––––––––––––––––––––––––––––––");
+    }
 }
