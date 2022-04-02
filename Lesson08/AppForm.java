0b241d749c6f758d6cf2700ff96c6af7c2c3ea69
[rrenat358] <[rrenat358@gmail.com]>
2022-04-02 15:51:59 +0300

    fontArial32B

--- Lesson08/AppForm.java
+++ Lesson08/AppForm.java
@@ -1,6 +1,7 @@
 package Lesson08;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,7 +13,12 @@ public class AppForm extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(500, 500, 450, 300);
 
+        setLayout(new GridLayout(3,3));
+        Font fontArial32B = new Font("Arial", Font.BOLD, 32);
+
+
         JButton button01 = new JButton("Нажми сюда");
+        button01.setFont(fontArial32B);
         add(button01);
 
         button01.addActionListener(new ActionListener() {
